d4862631b0b57bf4eebd3b1480ac515721616aba
Asyraf Omar <asyrafomar68@gmail.com>
2023-11-24 13:15:23 +0800

    addd multilinear documentation

--- WebApp/javascripts/inputvalues.js
+++
@@ -1,8 +0,0 @@
-const value = document.querySelector("#RiceProdVal");
-const input = document.querySelector("#RiceProduced");
-
-value.textContent = input.value
-input.addEventListener("input", (event) => {
-    value.textContent = event.target.value;
-  });
-
